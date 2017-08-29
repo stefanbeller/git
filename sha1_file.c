ac17eaee0783d81f31f7d8ef18e9154f6d3710c2
Jonathan Nieder <jrnieder@gmail.com>
2017-08-29 12:44:43 -0700

    sha1_file: add repository argument to check_tree
    Add a repository argument to allow the check_tree caller to be more
    specific about which repository to act on. This is a small mechanical
    change; it doesn't change the implementation to handle repositories
    other than the_repository yet.
    
    As with the previous commits, use a macro to catch callers passing a
    repository other than the_repository at compile time.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- sha1_file.c
+++ sha1_file.c
@@ -1739,7 +1739,8 @@ int has_object_file_with_flags(const struct object_id *oid, int flags)
 	return has_sha1_file_with_flags(oid->hash, flags);
 }
 
-static void check_tree(const void *buf, size_t size)
+#define check_tree(r, b, s) check_tree_##r(b, s)
+static void check_tree_the_repository(const void *buf, size_t size)
 {
 	struct tree_desc desc;
 	struct name_entry entry;
@@ -1794,7 +1795,7 @@ static int index_mem_the_repository(struct object_id *oid,
 	}
 	if (flags & HASH_FORMAT_CHECK) {
 		if (type == OBJ_TREE)
-			check_tree(buf, size);
+			check_tree(the_repository, buf, size);
 		if (type == OBJ_COMMIT)
 			check_commit(the_repository, buf, size);
 		if (type == OBJ_TAG)
