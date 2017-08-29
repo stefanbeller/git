569781866cb036adc9d9f9e9fcd2c9e8fea43099
Jonathan Nieder <jrnieder@gmail.com>
2017-08-29 12:42:24 -0700

    sha1_file: add repository argument to check_tag
    Add a repository argument to allow the check_tag callers to be more
    specific about which repository to act on. This is a small mechanical
    change; it doesn't change the implementation to handle repositories
    other than the_repository yet.
    
    As with the previous commits, use a macro to catch callers passing a
    repository other than the_repository at compile time.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- sha1_file.c
+++ sha1_file.c
@@ -1759,7 +1759,8 @@ static void check_commit(const void *buf, size_t size)
 		die("corrupt commit");
 }
 
-static void check_tag(const void *buf, size_t size)
+#define check_tag(r, b, s) check_tag_##r(b, s)
+static void check_tag_the_repository(const void *buf, size_t size)
 {
 	struct tag t;
 	memset(&t, 0, sizeof(t));
@@ -1796,7 +1797,7 @@ static int index_mem_the_repository(struct object_id *oid,
 		if (type == OBJ_COMMIT)
 			check_commit(buf, size);
 		if (type == OBJ_TAG)
-			check_tag(buf, size);
+			check_tag(the_repository, buf, size);
 	}
 
 	if (write_object)
