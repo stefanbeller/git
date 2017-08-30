9a7443127b9b9d56ef6cb835a0c1be33749931f9
Stefan Beller <sbeller@google.com>
2017-08-30 12:24:15 -0700

    replace-object: add repository argument to do_lookup_replace_object
    Add a repository argument to allow the do_lookup_replace_object caller
    to be more specific about which repository to handle. This is a small
    mechanical change; it doesn't change the implementation to handle
    repositories other than the_repository yet.
    
    As with the previous commits, use a macro to catch callers passing a
    repository other than the_repository at compile time.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- replace-object.h
+++ replace-object.h
@@ -13,7 +13,8 @@ struct replace_object {
  * This internal function is only declared here for the benefit of
  * lookup_replace_object().  Please do not call it directly.
  */
-extern const unsigned char *do_lookup_replace_object(const unsigned char *sha1);
+#define do_lookup_replace_object(r, s) do_lookup_replace_object_##r(s)
+extern const unsigned char *do_lookup_replace_object_the_repository(const unsigned char *sha1);
 
 /*
  * If object sha1 should be replaced, return the replacement object's
@@ -25,7 +26,7 @@ static inline const unsigned char *lookup_replace_object(const unsigned char *sh
 {
 	if (!check_replace_refs)
 		return sha1;
-	return do_lookup_replace_object(sha1);
+	return do_lookup_replace_object(the_repository, sha1);
 }
 
 #endif /* REPLACE_OBJECT_H */
