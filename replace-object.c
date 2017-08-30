90d6cf1f72e0ce05bc381f1cf119d6371ac9adf4
Stefan Beller <sbeller@google.com>
2017-08-30 11:05:37 -0700

    replace-object: move replace_object to object store
    Refs belong to particular repositories, so the replacements defined by them
    should belong to a particular repository as well.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- replace-object.c
+++ replace-object.c
@@ -1,18 +1,9 @@
 #include "cache.h"
+#include "replace-object.h"
 #include "sha1-lookup.h"
 #include "refs.h"
+#include "repository.h"
 #include "commit.h"
-
-/*
- * An array of replacements.  The array is kept sorted by the original
- * sha1.
- */
-static struct replace_object {
-	unsigned char original[20];
-	unsigned char replacement[20];
-} **replace_object;
-
-static int replace_object_alloc, replace_object_nr;
 
 static const unsigned char *replace_sha1_access(size_t index, void *table)
 {
@@ -22,7 +13,8 @@ static const unsigned char *replace_sha1_access(size_t index, void *table)
 
 static int replace_object_pos(const unsigned char *sha1)
 {
-	return sha1_pos(sha1, replace_object, replace_object_nr,
+	return sha1_pos(sha1, the_repository->objects.replacements.items,
+			the_repository->objects.replacements.nr,
 			replace_sha1_access);
 }
 
@@ -35,20 +27,22 @@ static int register_replace_object(struct replace_object *replace,
 		if (ignore_dups)
 			free(replace);
 		else {
-			free(replace_object[pos]);
-			replace_object[pos] = replace;
+			free(the_repository->objects.replacements.items[pos]);
+			the_repository->objects.replacements.items[pos] = replace;
 		}
 		return 1;
 	}
 	pos = -pos - 1;
-	ALLOC_GROW(replace_object, replace_object_nr + 1, replace_object_alloc);
-	replace_object_nr++;
-	if (pos < replace_object_nr)
-		memmove(replace_object + pos + 1,
-			replace_object + pos,
-			(replace_object_nr - pos - 1) *
-			sizeof(*replace_object));
-	replace_object[pos] = replace;
+	ALLOC_GROW(the_repository->objects.replacements.items,
+		   the_repository->objects.replacements.nr + 1,
+		   the_repository->objects.replacements.alloc);
+	the_repository->objects.replacements.nr++;
+	if (pos < the_repository->objects.replacements.nr)
+		memmove(the_repository->objects.replacements.items + pos + 1,
+			the_repository->objects.replacements.items + pos,
+			(the_repository->objects.replacements.nr - pos - 1) *
+			sizeof(*the_repository->objects.replacements.items));
+	the_repository->objects.replacements.items[pos] = replace;
 	return 0;
 }
 
@@ -86,7 +80,7 @@ static void prepare_replace_object(void)
 
 	for_each_replace_ref(register_replace_ref, NULL);
 	replace_object_prepared = 1;
-	if (!replace_object_nr)
+	if (!the_repository->objects.replacements.nr)
 		check_replace_refs = 0;
 }
 
@@ -115,7 +109,7 @@ const unsigned char *do_lookup_replace_object(const unsigned char *sha1)
 
 		pos = replace_object_pos(cur);
 		if (0 <= pos)
-			cur = replace_object[pos]->replacement;
+			cur = the_repository->objects.replacements.items[pos]->replacement;
 	} while (0 <= pos);
 
 	return cur;
