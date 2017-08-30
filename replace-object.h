a801eb20e70b78ddd5cdff33987086afb7f7b435
Stefan Beller <sbeller@google.com>
2017-08-30 15:31:03 -0700

    repository: allow lookup_replace_object to handle arbitrary repositories
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- replace-object.h
+++ replace-object.h
@@ -21,14 +21,14 @@ extern const unsigned char *do_lookup_replace_object(struct repository *r, const
  * either sha1 or a pointer to a permanently-allocated value.  When
  * object replacement is suppressed, always return sha1.
  */
-#define lookup_replace_object(r, s) lookup_replace_object_##r(s)
-static inline const unsigned char *lookup_replace_object_the_repository(const unsigned char *sha1)
+static inline const unsigned char *lookup_replace_object(struct repository *r,
+							 const unsigned char *sha1)
 {
 	if (!check_replace_refs ||
-	    (the_repository->objects.replacements.prepared &&
-	     the_repository->objects.replacements.nr == 0))
+	    (r->objects.replacements.prepared &&
+	     r->objects.replacements.nr == 0))
 		return sha1;
-	return do_lookup_replace_object(the_repository, sha1);
+	return do_lookup_replace_object(r, sha1);
 }
 
 #endif /* REPLACE_OBJECT_H */
