9f06472e222215cfae8db706e08652abbe480aa3
Jonathan Nieder <jrnieder@gmail.com>
2017-09-12 11:41:28 -0700

    object: add repository argument to lookup_commit_reference
    Add a repository argument to allow callers of lookup_commit_reference
    to be more specific about which repository to handle. This is a small
    mechanical change; it doesn't change the implementation to handle
    repositories other than the_repository yet.
    
    As with the previous commits, use a macro to catch callers passing a
    repository other than the_repository at compile time.
    
    The included coccinelle semantic patch will adapt any new callers in
    the diff produced by `make coccicheck`.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- builtin/rev-parse.c
+++ builtin/rev-parse.c
@@ -280,8 +280,8 @@ static int try_difference(const char *arg)
 		if (symmetric) {
 			struct commit_list *exclude;
 			struct commit *a, *b;
-			a = lookup_commit_reference(&oid);
-			b = lookup_commit_reference(&end);
+			a = lookup_commit_reference(the_repository, &oid);
+			b = lookup_commit_reference(the_repository, &end);
 			exclude = get_merge_bases(a, b);
 			while (exclude) {
 				struct commit *commit = pop_commit(&exclude);
@@ -333,7 +333,7 @@ static int try_parent_shorthands(const char *arg)
 		return 0;
 	}
 
-	commit = lookup_commit_reference(&oid);
+	commit = lookup_commit_reference(the_repository, &oid);
 	if (exclude_parent &&
 	    exclude_parent > commit_list_count(commit->parents)) {
 		*dotdot = '^';
