9152779a120bfbebea1f87fee0f1a81a2656f7d6
Stefan Beller <sbeller@google.com>
2017-09-12 11:49:57 -0700

    tag: add repository argument to lookup_tag
    Add a repository argument to allow the callers of lookup_tag
    to be more specific about which repository to act on. This is a small
    mechanical change; it doesn't change the implementation to handle
    repositories other than the_repository yet.
    
    As with the previous commits, use a macro to catch callers passing a
    repository other than the_repository at compile time.
    
    Add the cocci patch that converted the callers.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- object.c
+++ object.c
@@ -220,7 +220,7 @@ struct object *parse_object_buffer(const struct object_id *oid, enum object_type
 			obj = &commit->object;
 		}
 	} else if (type == OBJ_TAG) {
-		struct tag *tag = lookup_tag(oid);
+		struct tag *tag = lookup_tag(the_repository, oid);
 		if (tag) {
 			if (parse_tag_buffer(tag, buffer, size))
 			       return NULL;
