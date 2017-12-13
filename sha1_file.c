7b4973e4732f4cebd2bb060f5e2818ecb00bbe5b
Stefan Beller <sbeller@google.com>
2017-12-12 16:24:59 -0800

    alternates: convert add_to_alternates_memory to handle arbitrary repos
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- sha1_file.c
+++ sha1_file.c
@@ -581,16 +581,15 @@ void add_to_alternates_file_the_repository(const char *reference)
 	free(alts);
 }
 
-void add_to_alternates_memory_the_repository(const char *reference)
+void add_to_alternates_memory(struct repository *r, const char *reference)
 {
 	/*
 	 * Make sure alternates are initialized, or else our entry may be
 	 * overwritten when they are.
 	 */
-	prepare_alt_odb(the_repository);
+	prepare_alt_odb(r);
 
-	link_alt_odb_entries(the_repository, reference,
-			     '\n', NULL, 0);
+	link_alt_odb_entries(r, reference, '\n', NULL, 0);
 }
 
 /*
