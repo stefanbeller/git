3e7c2fa64117610b9917c7915c51ec02be925d64
Stefan Beller <sbeller@google.com>
2018-01-08 17:12:08 -0800

    commit: allow parse_commit_gently to handle arbitrary repository
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- commit.h
+++ commit.h
@@ -68,8 +68,7 @@ struct commit *lookup_commit_reference_by_name_the_repository(const char *name);
 struct commit *lookup_commit_or_die_the_repository(const struct object_id *oid, const char *ref_name);
 
 int parse_commit_buffer(struct repository *r, struct commit *item, const void *buffer, unsigned long size);
-#define parse_commit_gently(r, i, q) parse_commit_gently_##r(i, q)
-int parse_commit_gently_the_repository(struct commit *item, int quiet_on_missing);
+int parse_commit_gently(struct repository *r, struct commit *item, int quiet_on_missing);
 #define parse_commit(r, i) parse_commit_##r(i)
 static inline int parse_commit_the_repository(struct commit *item)
 {
