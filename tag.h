cfc00f5cf159684eb33d3d85bc0b3c2d90c4def9
Stefan Beller <sbeller@google.com>
2018-01-12 15:54:45 -0800

    tag: allow deref_tag to handle arbitrary repositories
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- tag.h
+++ tag.h
@@ -14,8 +14,7 @@ struct tag {
 extern struct tag *lookup_tag(struct repository *r, const struct object_id *oid);
 extern int parse_tag_buffer(struct repository *r, struct tag *item, const void *data, unsigned long size);
 extern int parse_tag(struct tag *item);
-#define deref_tag(r, o, w, l) deref_tag_##r(o, w, l)
-extern struct object *deref_tag_the_repository(struct object *, const char *, int);
+extern struct object *deref_tag(struct repository *r, struct object *, const char *, int);
 #define deref_tag_noverify(r, o) deref_tag_noverify_##r(o)
 extern struct object *deref_tag_noverify_the_repository(struct object *);
 #define gpg_verify_tag(r, o, n, f) gpg_verify_tag_##r(o, n, f)
