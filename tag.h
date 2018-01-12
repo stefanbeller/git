28e00dcec42b49ca894726837d355759c3df53e7
Stefan Beller <sbeller@google.com>
2018-01-12 15:40:58 -0800

    tag: allow parse_tag_buffer to handle arbitrary repositories
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- tag.h
+++ tag.h
@@ -12,8 +12,7 @@ struct tag {
 	timestamp_t date;
 };
 extern struct tag *lookup_tag(struct repository *r, const struct object_id *oid);
-#define parse_tag_buffer(r, i, d, s) parse_tag_buffer_##r(i, d, s)
-extern int parse_tag_buffer_the_repository(struct tag *item, const void *data, unsigned long size);
+extern int parse_tag_buffer(struct repository *r, struct tag *item, const void *data, unsigned long size);
 extern int parse_tag(struct tag *item);
 #define deref_tag(r, o, w, l) deref_tag_##r(o, w, l)
 extern struct object *deref_tag_the_repository(struct object *, const char *, int);
