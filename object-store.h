be287f7823568a147775eb12a22c17ba8f314e65
Stefan Beller <sbeller@google.com>
2018-02-12 12:50:27 -0800

    object-store: move alt_odb_list and alt_odb_tail to object store
    In a process with multiple repositories open, alternates should be
    associated to a single repository and not shared globally. Move
    alt_odb_list and alt_odb_tail into the_repository and adjust callers
    to reflect this.
    
    Now that the alternative object data base is per repository, we're
    leaking its memory upon freeing a repository. The next patch plugs
    this hole.
    
    No functional change intended.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- object-store.h
+++ object-store.h
@@ -1,14 +1,19 @@
 #ifndef OBJECT_STORE_H
 #define OBJECT_STORE_H
 
+#include "cache.h"
+
 struct raw_object_store {
 	/*
 	 * Path to the repository's object store.
 	 * Cannot be NULL after initialization.
 	 */
 	char *objectdir;
+
+	struct alternate_object_database *alt_odb_list;
+	struct alternate_object_database **alt_odb_tail;
 };
-#define RAW_OBJECT_STORE_INIT { NULL }
+#define RAW_OBJECT_STORE_INIT { NULL, NULL, NULL }
 
 void raw_object_store_clear(struct raw_object_store *o);
 
