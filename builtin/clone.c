04222f642d95d7ca4e006ca1c2086e4cc8d7858e
Stefan Beller <sbeller@google.com>
2018-02-15 14:55:34 -0800

    object-store: close all packs upon clearing the object store
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- builtin/clone.c
+++ builtin/clone.c
@@ -1217,7 +1217,7 @@ int cmd_clone(int argc, const char **argv, const char *prefix)
 	transport_disconnect(transport);
 
 	if (option_dissociate) {
-		close_all_packs();
+		close_all_packs(&the_repository->objects);
 		dissociate_from_references();
 	}
 
