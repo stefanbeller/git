005d07d8496d33ee5bac1534e0b6c05f8025dbbd
Stefan Beller <sbeller@rodete-laptop-imager.corp.google.com>
2018-07-31 11:38:13 -0700

    first way point for script/report

---
+++ data.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import subprocess
+import os
+
+gitrepo = os.getenv("HOME") + "/git"
+gitml =   os.getenv("HOME") + "/git-ml"
+
+sep="MYSEPARATOR"
+
+def strip_subject_line(s):
+	if s.startswith("[PATCH"):
+		p = s.find("]")+2  # account for } and ws after.
+		s = s[p:]
+	#strip off [PATCH ..]
+	# strip of "RE: " ?
+	return s
+
+def run_log_in(where):
+	cmd = ['git', '--no-pager', '-C', where,  'log',  '--no-merges', '--date=iso', '--format="%ad' +sep + '%an' + sep + '%s"']
+	p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+	r = {}
+	while p.returncode is None:
+		for line in p.communicate()[0].split("\n"):
+			if len(line) < 1:
+				continue
+			try:
+				(d, a, s) = line.split(sep)
+				ss = strip_subject_line(s)
+				if not ss in r:
+					r[ss] = []
+				r[ss] += [(d, a)]
+			except Exception as e:
+				print "X",line, "X"
+	return r
+
+print "checking repo"
+data_in_repo = run_log_in(gitrepo);
+print "reading ml"
+data_in_ml = run_log_in(gitml);
+
+nonuniquecounter=0
+dates = []
+mlmatches = {}
+
+for item in data_in_repo:
+	if len(data_in_repo[item]) != 1:
+		nonuniquecounter = nonuniquecounter+1
+		# ~ print data_in_repo[item], item 
+	else:
+		if not item in data_in_ml:
+			m = 0
+		else: 
+			m = len(data_in_ml[item])
+			
+		if not m in mlmatches:
+			mlmatches[m] = 0
+		
+		mlmatches[m] = mlmatches[m] + 1
+		
+print "non-unique subject lines", nonuniquecounter
+
+for x in mlmatches:
+	print x, mlmatches[x]
