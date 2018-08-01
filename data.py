ddce9c1c058fad1ab7473ecfaaa3465283747c22
Stefan Beller <sbeller@google.com>
2018-08-01 15:24:29 -0700

    first graph

--- data.py
+++ data.py
@@ -5,6 +5,12 @@ import os
 from datetime import datetime
 from datetime import timedelta
 
+
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
 gitrepo = os.getenv("HOME") + "/git"
 gitml =   os.getenv("HOME") + "/git-ml"
 
@@ -19,7 +25,7 @@ def strip_subject_line(s):
 	return s
 
 def run_log_in(where, args):
-	cmd = ['git', '--no-pager', '-C', where,  'log',  '--no-merges', ] + args + [ '--date=iso', '--format=%ad' +sep + '%an' + sep + '%s']
+	cmd = ['git', '--no-pager', '-C', where,  'log',  '--no-merges', ] + args + [ '--date=iso', '--format=%cd'+sep+'%ad' +sep + '%an' + sep + '%s']
 	p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 	r = {}
 	while p.returncode is None:
@@ -27,19 +33,33 @@ def run_log_in(where, args):
 			if len(line) < 1:
 				continue
 			try:
-				(d, a, s) = line.split(sep)
+				(cd, ad, a, s) = line.split(sep)
 				ss = strip_subject_line(s)
 				if not ss in r:
 					r[ss] = []
-				r[ss] += [(d, a)]
+				r[ss] += [(cd, ad, a)]
 			except Exception as e:
 				print "Problem with line X",line, "X"
 	return r
 
 print "checking repo"
-data_in_repo = run_log_in(gitrepo, ['--since=2011']);
+data_in_repo = run_log_in(gitrepo, ['--since=2005']);
 print "reading ml"
 data_in_ml = run_log_in(gitml, []);
+
+graphx={}
+
+def get_slot(at):
+	m = ((at.month + 2) / 3)  * 3
+	return datetime(at.year,m, 1)
+
+def add_to_slot(applied_time, timedelta):
+	x = get_slot(applied_time)
+	y = timedelta
+
+	if not x in graphx:
+		graphx[x] = []
+	graphx[x] += [y]
 
 nonuniquecounter=0
 dates = []
@@ -51,27 +71,60 @@ for item in data_in_repo:
 	else:
 		if not item in data_in_ml:
 			m = 0
-		else: 
+		else:
 			m = len(data_in_ml[item])
-			
+
 		if not m in mlmatches:
 			mlmatches[m] = 0
-		
+
 		mlmatches[m] = mlmatches[m] + 1
 		if m == 0:
 			continue
-			
+
 		# find the earliest appearance on the mailing list
 		youngest = datetime.now()
 		for i in data_in_ml[item]:
-			d = datetime.strptime(i[0][:19], '%Y-%m-%d %H:%M:%S')
+			# use author date!
+			d = datetime.strptime(i[1][:19], '%Y-%m-%d %H:%M:%S')
 			if d < youngest:
 				youngest = d
+
+		# committer date!
 		applied = datetime.strptime(data_in_repo[item][0][0][:19], '%Y-%m-%d %H:%M:%S')
 		t = (applied - youngest)
-		print applied, t
-		
-print "non-unique subject lines", nonuniquecounter
 
+		add_to_slot(applied, t)
+
+print "non-unique subject lines", nonuniquecounter, "ignored for further analysis"
+print "for reference there are", len(data_in_repo) , " in the repo dataset"
+print "and", len(data_in_ml), "in the ml data set"
+print
+print "occurrences on the mailing list, by commits"
+print
 for x in mlmatches:
 	print x, mlmatches[x]
+
+print "now plotting a graph"
+
+def percentile(lst, p):
+	as_days = [x.total_seconds() / 86400.0 for x in lst]
+	idx = int((0.01 * p) * len(as_days))
+	return sorted(as_days)[idx]
+
+x = sorted(graphx.keys())
+y1 = range(len(graphx))
+y2 = range(len(graphx))
+y3 = range(len(graphx))
+for i, xx in enumerate(x):
+	y1[i] = percentile(graphx[xx], 50)
+	y2[i] = percentile(graphx[xx], 70)
+	y3[i] = percentile(graphx[xx], 90)
+l1 = plt.plot_date(x, y1)
+l2 = plt.plot_date(x, y2,  color = "yellow")
+l3 = plt.plot_date(x, y3, color = "red")
+
+plt.title("date difference between first hit on ml and commit date in git.git")
+plt.ylabel("days")
+plt.legend((l1[0], l2[0], l3[0]), ('50 pctl', '70 pctl', '90 pctl'))
+plt.show()
+
